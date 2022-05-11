a7063bf0fa46b3cb392b7365bd24d52420a26b21
FlameZ <15273128925@163.com>
2022-05-11 11:38:52 +0800

    剑指offerⅡ

--- dp/hard/P_466统计重复个数.java
+++ dp/hard/P_466统计重复个数.java
@@ -7,46 +7,10 @@ public class P_466统计重复个数 {
     public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
         if(!isValid(s1,s2))
             return 0;
-        int[] info = getEpoch(s1, s2);
-//        System.out.println(info[0]+" "+ info[1]+" "+info[2] +" "+info[3]+" "+info[4]);
-        //先处理非循环段
-        int pre = info[3];
-        if(info[2] != 0)
-            pre++;
-        if(n1 == pre) return info[4]/n2;
-        if(n1<pre)
-            return n1*info[4]/(pre*n2);
-        int ret = info[4]/n2;
-        int premod = info[4]%n2;
-        if(ret == 0){
-            premod = n2 - premod;//非循环段欠出来的s2数量
-        }
-        //再处理循环段
-        n1 -= pre;
-        int cnt = n1/info[0];//s1可以完整对几轮
-        if(cnt == 0)//s1对不了完整的一轮
-            return ret + (int)((double)n1*info[1]/(info[0]*n2) - (double)premod/n2);
-        int mod = n1%info[0];//剩余的s1的数量
-        int ccnt = (cnt*info[1] - premod)/n2;//s2*n2可以完整对几轮
-        if(ccnt == 0)//s2对不了完整的一轮
-            return ret + (int)((double)n1*info[1]/(info[0]*n2) - (double)premod/n2);
-        int mmod = (cnt*info[1] - premod)%n2;//对齐ccnt后多出来的几个s2. 可能为零
-        if(mmod == 0){
-            if((mod*info[1]/info[0])>=n2)
-                ccnt++;
-        }else{
-            double cur = (double) mod/mmod;
-            double fix = (double) info[0]/info[1];
-            if(cur < fix)
-                ccnt--;
-        }
-        return ret + ccnt;
-    }
-    private int[] getEpoch(String s1, String s2){
-        int epoch = 0, cnt=0;
         int lenS1 = s1.length(), lenS2 = s2.length();
+        Map<Integer, int[]> map = new HashMap<>();
+        int epoch = 0, cnt=0, ret = 0;
         int i = 0, j = 0;
-        Map<Integer, int[]> map = new HashMap();
         while(!(!map.isEmpty() && j==0 && map.containsKey(i))){
             if(s1.charAt(i) == s2.charAt(j)){
                 if(map.containsKey(i) && j == 0) break;
@@ -60,15 +24,38 @@ public class P_466统计重复个数 {
             }
             if(++i>=lenS1){
                 i = 0;
-                epoch++;
+                if(++epoch == n1)//未到达循环节，直接返回现已匹配了的
+                    return cnt/n2;
             }
         }
-        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
-            System.out.println(entry.getKey() +" "+entry.getValue()[0]+" "+entry.getValue()[1]);
+//        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+//            System.out.print(entry.getKey() +" "+entry.getValue()[0]+" "+entry.getValue()[1]);
+//            System.out.println();
+//        }
+        int[] cur = map.get(i); //前一个循环节信息
+        epoch  -= cur[0]; //循环节中s1个数
+        cnt -= cur[1];//循环节中s2个数
+
+        n1 -= cur[0]; //循环部分总长度
+        n1--;//剪掉开头的那部分
+        int times = n1/epoch; //循环节数量
+        int resdual = n1%epoch+1; //剩余s1的数量，加1是要加上之前剪掉的那部分
+        int more = 0;
+        while(resdual > 0){
+            if(s1.charAt(i) == s2.charAt(j)){
+                j++;
+                if(j>=s2.length()){
+                    j=0;
+                    more++;
+                }
+            }
+            i++;
+            if(i>=s1.length()){
+                i=0;
+                resdual--;
+            }
         }
-//        epoch--;
-        int[] cur = map.get(i);
-        return new int[]{epoch-cur[0], cnt-cur[1], i, cur[0], cur[1]};
+        return (cur[1] + times*cnt + more)/n2;
     }
 
     private boolean isValid(String s1, String s2){
